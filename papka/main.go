e3543d890fe1523aadf57322a38cfeb0f0c7157d
Sparkmoons <82277295+Sparkmoons@users.noreply.github.com>
2024-03-17 20:20:10 +0300

    Update main.go

--- papka/main.go
+++ papka/main.go
@@ -21,6 +21,10 @@ func main() {
 		log.Fatal(err)
 	}
 
+	if err := createTables(); err != nil {
+		log.Fatal(err)
+	}
+	
 	http.HandleFunc("actors", GetActors)
 	http.HandleFunc("/actors/add", AddActor)
 	http.HandleFunc("/actors/update", UpdActor)
@@ -35,3 +39,42 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+func createTables() error {
+	_, err := db.Exec(`
+		CREATE TABLE IF NOT EXISTS actors (
+			id SERIAL PRIMARY KEY,
+			name VARCHAR(150) NOT NULL,
+			gender VARCHAR(10),
+			birth DATE
+		)
+	`)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS movies (
+			id SERIAL PRIMARY KEY,
+			name VARCHAR(150) NOT NULL,
+			description TEXT,
+			release DATE,
+			rate INT CHECK (rate >= 0 AND rate <= 10)
+		)
+	`)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS movie_actors (
+			movie_id INT REFERENCES movies(id),
+			actor_id INT REFERENCES actor(id),
+			PRIMARY KEY (movie_id, actor_id)
+		)
+	`)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
